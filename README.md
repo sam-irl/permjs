da837712491468baf557fc67350f1695a9332bdf
sam-irl <therealsamave@gmail.com>
2018-06-04 23:42:51 +0100

    Initial commit

---
+++ README.md
@@ -0,0 +1,74 @@
+# permjs
+
+A library providing mathematical permutation, combination, and factorial operations.
+
+## Installation
+
+Using npm:
+
+```
+npm install --save permjs
+```
+
+## Usage
+### Permutation
+#### Purpose
+Finds the possible orders of n objects taken r at a time.
+#### Mathmatical formula
+```
+nPr = n!/(n - r)!
+```
+#### Implementation
+```javascript
+var permjs = require('permjs');
+
+permjs.permutation(5, 2); // 5 nPr 2 => 20
+```
+### Combination
+#### Purpose
+Finds the possible sets (without regard to order) of n objects taken r at a time.
+#### Mathematical Formula
+```
+nCr = n!/(n - r)!r!
+```
+#### Implementation
+```javascript
+var permjs = require('permjs');
+
+permjs.combination(5, 2); // 5 nCr 2 => 10
+```
+### Factorial
+#### Purpose
+Finds a number multipled by all integers smaller than it, down to 1. For example,
+
+5! = 5 * 4 * 3 * 2 * 1 = 120
+#### Mathematical Formula
+```
+n! = n * (n - 1) * (n - 2) ... * 1
+```
+#### Implementation
+```javascript
+var permjs = require('permjs');
+
+permjs.factorial(5); // 5! => 120
+```
+### Pascal's Triangle
+#### Purpose
+Gives the binomial coefficients of the binomial power (a + b)^n
+#### Mathematical Formula
+Takes the following form:
+```
+row[0]:          1
+row[1]:         1 1
+row[2]:        1 2 1
+row[3]:       1 3 3 1
+row[4]:      1 4 6 4 1
+row[5]:    1 5 10 10 5 1
+```
+...etc
+#### Implementation
+```javascript
+var permjs = require('permjs');
+
+permjs.pascal(5); // row[5] => [1, 5, 10, 10, 5, 1]
+```
---
+++ index.js
@@ -0,0 +1,122 @@
+/**
+ * Calculates the permutation of the number of elements, in the form nPr.
+ * 
+ * For example, nPr becomes permutation(n, r) - so 5P8 is permutation(5, 8).
+ * 
+ * @param {Number} n - size of set
+ * @param {Number} r - items to select
+ * @returns {Number|NaN} the possible permutations
+ */
+function permutation(n, r) {
+    if (!(typeof n === "number") || !(typeof r === "number")) {
+        throw TypeError('Cannot pass non-numeric arguments to permutation, did you mean to take the length?');
+    }
+    if (n < 0 || r < 0) {
+        return NaN;
+    }
+    var nFactorial = 1;
+    var nrFactorial = 1;
+    for (var i = n; i > 1; i--) {
+        nFactorial *= i;
+    }
+    for (var i = (n - r); i > 1; i--) {
+        nrFactorial *= i;
+    }
+    return nFactorial / nrFactorial;
+}
+
+/**
+ * Calculates the number of possible combinations of the elements, in the form nCr.
+ * 
+ * Similar to the permutation, but disregards order.
+ * 
+ * @param {Number} n - size of set
+ * @param {Number} r - items to select
+ * @returns {Number} the possible combinations
+ */
+function combination(n, r) {
+    if (!(typeof n === "number") || !(typeof r === "number")) {
+        throw TypeError('Cannot pass non-numeric arguments to combination, did you mean to take the length?');
+    }
+    if (n < 0 || r < 0) {
+        return NaN;
+    }
+    var nFactorial = 1;
+    var nrFactorial = 1;
+    var rFactorial = 1;
+    for (var i = n; i > 1; i--) {
+        nFactorial *= i;
+    }
+    for (var i = (n - r); i > 1; i--) {
+        nrFactorial *= i;
+    }
+    for (var i = r; i > 1; i--) {
+        rFactorial *= i;
+    }
+    return nFactorial / (nrFactorial * rFactorial);
+}
+
+/**
+ * Finds the factorial of the given number, n.
+ * 
+ * Multiplies n by n - 1, n - 2, etc, until it reaches 1.
+ * 
+ * @param {Number} n 
+ */
+function factorial(n) {
+    if (!(typeof n === "number")) {
+        throw TypeError('Cannot pass non-numeric arguments to factorial, did you mean to take the length?');
+    }
+    if (n < 0) {
+        return NaN;
+    }
+    var result = 1;
+    for (var i = n; i > 1; i--) {
+        result *= i;
+    }
+    return result;
+}
+
+/**
+ * Returns the nth row of Pascal's Triangle, given
+ * 
+ * row0 =       1
+ * 
+ * row1 =      1 1
+ * 
+ * row2 =     1 2 1
+ * 
+ * row3 =    1 3 3 1
+ * 
+ * row4 =   1 4 6 4 1
+ * 
+ * row5 = 1 5 10 10 5 1
+ * 
+ * etc
+ * 
+ * @param {Number} n
+ * @returns {Number[]} coefficients
+ */
+function pascal(n) {
+    if (!typeof n === 'Number') {
+        console.warn('pascal get non-number parameter, will attempt to cast to Number but may yield undesired results!');
+        n = Number(n);
+    }
+    if (n < 0) throw Error('Row number of Pascal\'s triangle can be, at minimum, 0, instead got ' + n);
+    if (n === 0) return [1];
+    var coeff = Array(n + 1);
+    for (var i = 0; i <= n; i++) {
+        coeff[i] = combination(n, i);
+    }
+    return coeff;
+}
+
+// Namespace
+var permjs = {
+    permutation: permutation,
+    combination: combination,
+    factorial: factorial,
+    pascal: pascal
+}
+
+module.exports = permjs;
